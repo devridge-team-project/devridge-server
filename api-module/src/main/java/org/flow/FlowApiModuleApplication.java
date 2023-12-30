fde47c08a7dbde2df4973e520de5cddca2ddeb1d
yu-heejin <jkgt0521@naver.com>
2023-12-30 20:03:40 +0900

    chore: 멀티 모듈 구조로 변경 (#1)

--- api-module/src/main/java/org/flow/FlowApiModuleApplication.java
+++ api-module/src/main/java/org/flow/FlowApiModuleApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FlowServerApplication {
+public class FlowApiModuleApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(FlowServerApplication.class, args);
+        SpringApplication.run(FlowApiModuleApplication.class, args);
     }
 }
