ef13d2eade362b51e49114991a8e98ce87ef1c9a
yu-heejin <jkgt0521@naver.com>
2024-01-02 01:32:46 +0900

    feat: BaseResponse 클래스 추가 (#1)

---
+++ common-module/src/main/java/org/flow/dto/BaseResponse.java
@@ -0,0 +1,22 @@
+package org.flow.dto;
+
+import lombok.Getter;
+
+@Getter
+public class BaseResponse<T> {
+
+    private int code;
+    private String message;
+    private T body;
+
+    public BaseResponse(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public BaseResponse(int code, String message, T data) {
+        this.code = code;
+        this.message = message;
+        this.body = data;
+    }
+}
