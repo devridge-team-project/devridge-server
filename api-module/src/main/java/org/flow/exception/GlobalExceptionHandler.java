eb2b66b4474b407a0c83db5942ccf3afdf501989
yu-heejin <jkgt0521@naver.com>
2024-01-02 20:42:03 +0900

    feat: GlobalExceptionHandler 추가 (#1)

---
+++ api-module/src/main/java/org/flow/exception/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package org.flow.exception;
+
+import org.flow.dto.BaseResponse;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.NoSuchElementException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<BaseResponse> handleElementNotFountException(NoSuchElementException e) {
+        BaseResponse response = new BaseResponse(HttpStatus.NOT_FOUND.value(), "해당 요소를 찾을 수 없습니다.");
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+}
