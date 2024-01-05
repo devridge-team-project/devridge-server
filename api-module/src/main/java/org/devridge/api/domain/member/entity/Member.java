cb09323905af417587b505aa5fcff824384006e3
matt <matt1235@naver.com>
2024-01-05 22:56:38 +0900

    feat : Member Entity 추가 (#2) Member 엔티티 추가 AbstractTimeEntity를 통해 공통되는 필드 묶음

---
+++ api-module/src/main/java/org/devridge/api/domain/member/entity/Member.java
@@ -0,0 +1,53 @@
+package org.devridge.api.domain.member.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.devridge.api.AbstractTimeEntity;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Entity
+@Table(name = "member")
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Member extends AbstractTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "provider")
+    private String provider;
+
+    @Column(name = "salt", nullable = false)
+    @Type(type = "uuid-char")
+    private UUID salt;
+
+    @Column(name = "nickname", nullable = false)
+    private String nickname;
+
+    @Column(name = "roles", nullable = false)
+    private String roles;
+
+    @Column(name = "introduction", nullable = true)
+    private String introduction;
+
+    @Column(name = "profile_image_url", nullable = true)
+    private String profileImageUrl;
+
+    private boolean isDeleted;
+
+}
