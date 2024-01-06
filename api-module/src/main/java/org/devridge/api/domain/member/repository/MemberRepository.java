30ba2f8fdb132240abb7524e65ea674200a56e29
matt <matt1235@naver.com>
2024-01-06 23:04:42 +0900

    feat : logout 핸들러 구현, member 클래스 예외처리 (#2)
    CustomLogoutHandler
    - 로그아웃 시, 실행되는 로직 구현

--- api-module/src/main/java/org/devridge/api/domain/member/repository/MemberRepository.java
+++ api-module/src/main/java/org/devridge/api/domain/member/repository/MemberRepository.java
@@ -10,4 +10,6 @@ import java.util.Optional;
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     public Optional<Member> findByEmail(String email);
+
+    Optional<Member> findByEmailAndProvider(String email, String provider);
 }
---
+++ api-module/src/main/java/org/devridge/api/security/handler/CustomLogoutHandler.java
@@ -0,0 +1,26 @@
+package org.devridge.api.security.handler;
+
+import org.devridge.api.util.ResponseUtil;
+import org.devridge.common.dto.BaseResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CustomLogoutHandler implements LogoutSuccessHandler {
+
+    @Override
+    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        BaseResponse baseResponse = new BaseResponse(
+                HttpStatus.OK.value(),
+                "logout success"
+        );
+
+        ResponseUtil.createResponseMessage(response, baseResponse);
+    }
+}
