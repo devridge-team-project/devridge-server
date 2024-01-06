7d1e78e2279dc7e4f1b4ec55c71db1e02e7ac9cb
matt <matt1235@naver.com>
2024-01-06 22:06:27 +0900

    feat : jwt 관련 설정 값, dto 클래스 구현 (#2)
    AuthProperties
    - 엑세스, 리프레시 토큰의 secret 값을 설정파일(yml)에서 가져온다.
    - 각 값은 서버에서만 알 수 있는 private한 값이다.
    
    TokenResponse
    - accessToken, refreshToken을 응답으로 제공할 때 사용하는 클래스

---
+++ api-module/src/main/java/org/devridge/api/security/dto/TokenResponse.java
@@ -0,0 +1,16 @@
+package org.devridge.api.security.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TokenResponse {
+
+    private String accessToken;
+    private String refreshToken;
+}
