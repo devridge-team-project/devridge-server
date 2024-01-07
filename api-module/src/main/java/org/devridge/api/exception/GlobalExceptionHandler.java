e87c05447e2042c7f79e4444b8c08f584373e65f
matt <matt1235@naver.com>
2024-01-08 05:27:39 +0900

    fix : 에러 핸들러에서 반환하는 코드(code) 값 수정 (#2) 404 -> 400 으로 수정

--- api-module/src/main/java/org/devridge/api/exception/GlobalExceptionHandler.java
+++ api-module/src/main/java/org/devridge/api/exception/GlobalExceptionHandler.java
@@ -22,19 +22,19 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(PasswordNotMatchException.class)
     public ResponseEntity<BaseResponse> handleException(PasswordNotMatchException e) {
-        BaseResponse response = new BaseResponse(HttpStatus.NOT_FOUND.value(), "비밀번호가 일치하지 않습니다.");
+        BaseResponse response = new BaseResponse(HttpStatus.BAD_REQUEST.value(), "비밀번호가 일치하지 않습니다.");
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(SkillsNotValidException.class)
     public ResponseEntity<BaseResponse> handleException(SkillsNotValidException e) {
-        BaseResponse response = new BaseResponse(HttpStatus.NOT_FOUND.value(), "사버에 존재하지 않는 직군이 포함되어 있습니다. 다시 확인 바랍니다.");
+        BaseResponse response = new BaseResponse(HttpStatus.BAD_REQUEST.value(), "사버에 존재하지 않는 직군이 포함되어 있습니다. 다시 확인 바랍니다.");
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(WeakPasswordException.class)
     public ResponseEntity<BaseResponse> handleException(WeakPasswordException e) {
-        BaseResponse response = new BaseResponse(HttpStatus.NOT_FOUND.value(), e.getMessage());
+        BaseResponse response = new BaseResponse(HttpStatus.BAD_REQUEST.value(), e.getMessage());
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 }
