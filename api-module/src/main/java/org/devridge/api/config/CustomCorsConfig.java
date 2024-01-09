264e0baa4405a90ff2cc4ee519ed290627e8d4f8
HaJungSu <56336436+Jungsu-lilly@users.noreply.github.com>
2024-01-09 21:54:34 +0900

    chore : 메서드 띄어쓰기 (코드 컨벤션)
    Co-authored-by: yu-heejin <96467030+yu-heejin@users.noreply.github.com>

--- api-module/src/main/java/org/devridge/api/config/CustomCorsConfig.java
+++ api-module/src/main/java/org/devridge/api/config/CustomCorsConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CustomCorsConfig implements WebMvcConfigurer {
 
     @Override
-    public void addCorsMappings(CorsRegistry registry){
+    public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:8000", "http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
