1acb15418334daba82c7428708541e67e9f919a9
213kky <213kky@naver.com>
2024-01-10 01:10:51 +0900

    chore: @Column 어노테이션 추가 (#13)

--- api-module/src/main/java/org/devridge/api/domain/community/Community.java
+++ api-module/src/main/java/org/devridge/api/domain/community/Community.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -12,16 +13,26 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.devridge.api.domain.communitycomment.CommunityComment;
 import org.devridge.api.domain.communityscrap.CommunityScrap;
 import org.devridge.api.githubsociallogintemp.domain.Member;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-
-@Entity
 @Getter
+@Setter // 나중에 지우기
+@NoArgsConstructor( access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Entity
+@EntityListeners(AuditingEntityListener.class)
 @Table(name = "community")
 public class Community {
     @Id
@@ -40,6 +51,7 @@ public class Community {
     private Long views;
 
     @CreatedDate
+    @Column(updatable = false)
     private LocalDateTime createdAt;
 
     @LastModifiedDate
--- api-module/src/main/java/org/devridge/api/domain/communitycomment/CommunityComment.java
+++ api-module/src/main/java/org/devridge/api/domain/communitycomment/CommunityComment.java
@@ -5,20 +5,32 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
-import org.devridge.api.domain.communitycommentlikedislike.CommunityCommentLikeDislike;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.devridge.api.domain.community.Community;
+import org.devridge.api.domain.communitycommentlikedislike.CommunityCommentLikeDislike;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Getter
+@Setter // 나중에 지우기
+@NoArgsConstructor( access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 public class CommunityComment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -39,6 +51,7 @@ public class CommunityComment {
     private Boolean isDeleted;
 
     @CreatedDate
+    @Column(updatable = false)
     private LocalDateTime createdAt;
 
     @LastModifiedDate
