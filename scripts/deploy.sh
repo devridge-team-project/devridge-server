23628b992ee69af2f6e23910e0dacd5e2f7e9ed1
yu-heejin <jkgt0521@naver.com>
2024-01-10 00:23:06 +0900

    feat: 무중단 배포를 위한 nginx.conf 추가 (#7)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,13 +9,13 @@ EXIST_BLUE=$(docker ps -f "name=dev-blue" -f "status=running" -q)
 # blue가 실행되지 않는다면 (green 실행 중)
 if [ -z "$EXIST_BLUE" ]; then
   # blue 컨테이너 배포 로그 생성
-  echo "blue container deploy: $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> "/home/${EC2_USERNAME}/server/deploy.log"
+  echo "blue container deploy: $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/${EC2_USERNAME}/server/deploy.log
   sudo docker run -d -p "${BLUE_PORT}:${SPRING_BASIC_PORT}" --rm --name dev-blue "${DOCKER_USERNAME}/${DOCKER_REPOSITORY_NAME}:${DOCKER_BLUE_NAME}"
   STOP_CONTAINER="green"
   START_CONTAINER="blue"
 else
-  echo "green container deploy: $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> "/home/${EC2_USERNAME}/server/deploy.log"
-  sudo docker run -d -p "${GREEN_PORT}:${SPRING_BASIC_PORT}" --rm --name dev-green "${DOCKER_USERNAME}/${DOCKER_REPOSITORY_NAME}:${DOCKER_GREEN_NAME}"
+  echo "green container deploy: $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/${EC2_USERNAME}/server/deploy.log
+  sudo docker run -d -p ${GREEN_PORT}:${SPRING_BASIC_PORT} --rm --name dev-green ${DOCKER_USERNAME}/${DOCKER_REPOSITORY_NAME}:${DOCKER_GREEN_NAME}
   STOP_CONTAINER="blue"
   START_CONTAINER="green"
 fi
@@ -26,6 +26,8 @@ sleep 10
 EXIST_AFTER=$(docker ps -f "name=dev-$START_CONTAINER" -f "status=running" -q)
 # 해당 컨테이너가 정상적으로 실행되는 경우
 if [ -n "$EXIST_AFTER" ]; then
+  cp /etc/nginx/nginx.${START_CONTAINER}.conf /etc/nginx/nginx.conf
+  sudo nginx -s reload
   # 이전 컨테이너 종료
   sudo docker stop "dev-$STOP_CONTAINER"
 fi
