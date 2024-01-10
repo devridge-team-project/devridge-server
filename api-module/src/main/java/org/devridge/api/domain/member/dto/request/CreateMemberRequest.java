866a0c4ab2819cc5cb2e037b6c64eed19196c3ae
matt <matt1235@naver.com>
2024-01-10 20:55:44 +0900

    chore : 회원가입 요청 dto에 유효성 검사하는 로직 추가 (#2)

--- api-module/src/main/java/org/devridge/api/domain/member/dto/request/CreateMemberRequest.java
+++ api-module/src/main/java/org/devridge/api/domain/member/dto/request/CreateMemberRequest.java
@@ -14,9 +14,11 @@ public class CreateMemberRequest {
     @NotBlank(message = "빈 이메일을 입력할 수 없습니다.")
     private String email;
 
+    @NotBlank(message = "빈 패스워드를 입력할 수 없습니다.")
     private String password;
 
-    private String provider = "normal";
+    @NotBlank(message = "빈 provider를 입력할 수 없습니다.")
+    private String provider;
 
     @NotBlank(message = "빈 닉네임을 입력할 수 없습니다.")
     private String nickname;
