1c77ad7f9493141abe3e829fba7032ecf62680bd
213kky <213kky@naver.com>
2024-01-15 00:47:28 +0900

    refactor: 커뮤니티 스크랩 api 반환 형식 수정 (#13)

--- api-module/src/main/java/org/devridge/api/domain/communityscrap/CommunityScrapController.java
+++ api-module/src/main/java/org/devridge/api/domain/communityscrap/CommunityScrapController.java
@@ -1,6 +1,8 @@
 package org.devridge.api.domain.communityscrap;
 
+import org.devridge.common.dto.BaseResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,13 +23,21 @@ public class CommunityScrapController {
     public ResponseEntity<?> scrapCreate(@PathVariable Long memberId,
         @PathVariable Long communityId) {
         communityScrapService.createScrap(memberId, communityId);
-        return ResponseEntity.ok().build();
+        BaseResponse response = new BaseResponse(
+            HttpStatus.OK.value(),
+            "스크랩 성공"
+        );
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @DeleteMapping("/community/scrap/{communityId}/{memberId}")
     public ResponseEntity<?> scrapdelete(@PathVariable Long memberId,
         @PathVariable Long communityId) {
         communityScrapService.deleteScrap(memberId, communityId);
-        return ResponseEntity.ok().build();
+        BaseResponse response = new BaseResponse(
+            HttpStatus.OK.value(),
+            "스크랩 삭제 성공"
+        );
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
