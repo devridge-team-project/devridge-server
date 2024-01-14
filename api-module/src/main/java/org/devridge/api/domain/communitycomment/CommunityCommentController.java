154ddeeeea1f00a0dce80ad83e5560eeb6eeb56e
213kky <213kky@naver.com>
2024-01-15 00:14:34 +0900

    refactor: 커뮤니티 댓글 api 반환 형식 수정 (#13)

--- api-module/src/main/java/org/devridge/api/domain/communitycomment/CommunityCommentController.java
+++ api-module/src/main/java/org/devridge/api/domain/communitycomment/CommunityCommentController.java
@@ -3,6 +3,7 @@ package org.devridge.api.domain.communitycomment;
 import java.util.ArrayList;
 import java.util.List;
 import org.devridge.api.githubsociallogintemp.domain.Member;
+import org.devridge.common.dto.BaseResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -27,27 +28,38 @@ public class CommunityCommentController {
     public ResponseEntity<?> writeComment(@RequestBody CommunityCommentRequest dto,
         @PathVariable Long communityId, @PathVariable Long memberId) {
         communityCommentService.createComment(communityId, memberId, dto.getContent());
-        return ResponseEntity.ok().build();
+        BaseResponse response = new BaseResponse(
+            HttpStatus.OK.value(),
+            "댓글 작성 성공"
+        );
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @GetMapping("/read/comment/community/{communityId}")
     public ResponseEntity<?> viewComment(@PathVariable Long communityId) {
-        List<CommunityComment> communityComments
-            = communityCommentService.getAllComment(communityId);
-        List<CommunityCommentResponse> dto = new ArrayList<>();
-
+        List<CommunityComment> communityComments = communityCommentService.getAllComment(communityId);
+        List<CommunityCommentResponse> dtos = new ArrayList<>();
         for (CommunityComment comment : communityComments) {
             Member member = comment.getMember(); // 댓글 작성자 정보 가져오기   //todo: 없는 맴버 안 가져오는 예외 처리
             CommunityCommentResponse communityCommentResponse = new CommunityCommentResponse(
                 member.getNickname(), comment.getUpdatedAt());
-            dto.add(communityCommentResponse);
+            dtos.add(communityCommentResponse);
         }
-        return ResponseEntity.ok(dto);
+        BaseResponse response = new BaseResponse(
+            HttpStatus.OK.value(),
+            "댓글 목록 조회 성공",
+            dtos
+        );
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @DeleteMapping("/delete/comment/community/{commentId}")
     public ResponseEntity<?> CommentDelete(@PathVariable Long commentId) {
         communityCommentService.deleteComment(commentId);
-        return ResponseEntity.ok(HttpStatus.OK);
+        BaseResponse response = new BaseResponse(
+            HttpStatus.OK.value(),
+            "댓글 삭제 성공"
+        );
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
