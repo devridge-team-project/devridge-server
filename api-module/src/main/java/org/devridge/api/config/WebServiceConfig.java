260e85a856d1d112b82a3fab8793150d08af9277
yu-heejin <jkgt0521@naver.com>
2024-01-19 21:36:06 +0900

    feat: 댓글 수정 및 삭제에 대한 인터셉터 추가 (#3)

--- api-module/src/main/java/org/devridge/api/config/WebServiceConfig.java
+++ api-module/src/main/java/org/devridge/api/config/WebServiceConfig.java
@@ -2,6 +2,7 @@ package org.devridge.api.config;
 
 import lombok.RequiredArgsConstructor;
 import org.devridge.api.domain.qna.interceptor.QnAAuthInterceptor;
+import org.devridge.api.domain.qna.interceptor.QnACommentAuthInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,10 +12,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebServiceConfig implements WebMvcConfigurer {
 
     private final QnAAuthInterceptor qnaAuthInterceptor;
+    private final QnACommentAuthInterceptor qnaCommentAuthInterceptor;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(qnaAuthInterceptor)
-                .addPathPatterns("/api/qna/**");
+            .excludePathPatterns("/api/qna/{qnaId}/comments/**")
+            .addPathPatterns("/api/qna/**");
+
+        registry.addInterceptor(qnaCommentAuthInterceptor)
+            .addPathPatterns("/api/qna/{qnaId}/comments/**");
     }
 }
