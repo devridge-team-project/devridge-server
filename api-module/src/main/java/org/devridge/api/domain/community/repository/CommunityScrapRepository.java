675d6eb0f3596bf1309f05ceb23c6a28fd8e0f8d
213kky <213kky@naver.com>
2024-01-23 19:28:31 +0900

    refactor: 스크랩 추가 시 softDelete case 처리 추가 (#13)

--- api-module/src/main/java/org/devridge/api/domain/community/repository/CommunityScrapRepository.java
+++ api-module/src/main/java/org/devridge/api/domain/community/repository/CommunityScrapRepository.java
@@ -1,11 +1,27 @@
 package org.devridge.api.domain.community.repository;
 
+import javax.transaction.Transactional;
 import org.devridge.api.domain.community.entity.CommunityScrap;
 import org.devridge.api.domain.community.entity.id.CommunityScrapId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CommunityScrapRepository extends JpaRepository<CommunityScrap, CommunityScrapId> {
 
+    @Modifying
+    @Transactional
+    @Query("UPDATE CommunityScrap cs "
+        + "SET cs.isDeleted = false "
+        + "WHERE cs.community.id = :communityId "
+        + "AND cs.member.id = :memberId")
+    void reCreateScrap(@Param("communityId") Long communityId, @Param("memberId") Long memberId);
+
+    @Query(value = "SELECT COUNT(*) FROM community_scrap " +
+        "WHERE community_id = :communityId " +
+        "AND member_id = :memberId ", nativeQuery = true)
+    Long checkSoftDelete(@Param("communityId") Long communityId, @Param("memberId") Long memberId);
 }
--- api-module/src/main/java/org/devridge/api/domain/community/service/CommunityScrapService.java
+++ api-module/src/main/java/org/devridge/api/domain/community/service/CommunityScrapService.java
@@ -2,9 +2,13 @@ package org.devridge.api.domain.community.service;
 
 import javax.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.devridge.api.domain.community.entity.Community;
 import org.devridge.api.domain.community.entity.CommunityScrap;
 import org.devridge.api.domain.community.entity.id.CommunityScrapId;
+import org.devridge.api.domain.community.repository.CommunityRepository;
 import org.devridge.api.domain.community.repository.CommunityScrapRepository;
+import org.devridge.api.domain.member.entity.Member;
+import org.devridge.api.domain.member.repository.MemberRepository;
 import org.devridge.api.util.SecurityContextHolderUtil;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
@@ -14,25 +18,59 @@ import org.springframework.stereotype.Service;
 public class CommunityScrapService {
 
     private final CommunityScrapRepository communityScrapRepository;
+    private final MemberRepository memberRepository;
+    private final CommunityRepository communityRepository;
 
     public void createScrap(Long communityId) {
-        try {
-            Long memberId = SecurityContextHolderUtil.getMemberId();
-            CommunityScrap communityScrap = CommunityScrap.builder()
-                .id(new CommunityScrapId(memberId, communityId))
-                .build();
-            communityScrapRepository.save(communityScrap);
-        } catch (DataIntegrityViolationException e) {
-            throw new DataIntegrityViolationException("이미 존재하는 데이터입니다.");
-        }
+        Long memberId = SecurityContextHolderUtil.getMemberId();
+        Member member = getMemberById(memberId);
+        Community community = getCommunityById(communityId);
+        CommunityScrapId communityScrapId = new CommunityScrapId(memberId, communityId);
 
+        communityScrapRepository.findById(communityScrapId)
+            .ifPresent(cs -> {
+                throw new DataIntegrityViolationException("이미 존재");
+            });
+
+        if (communityScrapRepository.findById(communityScrapId).isEmpty()) { // 없나확인
+            Long countResult = communityScrapRepository.checkSoftDelete(communityId, memberId); // 0이외의 값은 가짜없음
+            boolean isSoftDeleted = countResult != 0L;
+
+            //가짜없음
+            //reCreateScrap쿼리하기
+            if (isSoftDeleted) {  // 참이면 가짜없음임 즉 실행되면
+                communityScrapRepository.reCreateScrap(communityId, memberId);
+            }
+
+            //  진짜없음
+            //  새로만들기
+            if (!isSoftDeleted) {
+                CommunityScrap communityScrap = CommunityScrap.builder()
+                    .community(community)
+                    .member(member)
+                    .build();
+                communityScrapRepository.save(communityScrap);
+            }
+        }
     }
 
     public void deleteScrap(Long communityId) {
         Long memberId = SecurityContextHolderUtil.getMemberId();
+        Member member = getMemberById(memberId);
+        getCommunityById(communityId);
+
         CommunityScrapId communityScrapId = new CommunityScrapId(memberId, communityId);
         communityScrapRepository.findById(communityScrapId)
             .orElseThrow(() -> new EntityNotFoundException("스크랩이 존재하지 않습니다."));
         communityScrapRepository.deleteById(communityScrapId);
     }
+
+    public Member getMemberById(Long memberId) {
+        return memberRepository.findById(memberId).orElseThrow(() -> new EntityNotFoundException());
+    }
+
+    public Community getCommunityById(Long communityId) {
+        return communityRepository.findById(communityId).orElseThrow(() -> new EntityNotFoundException());
+    }
+
 }
