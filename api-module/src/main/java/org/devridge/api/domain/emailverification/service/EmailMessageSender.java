859773e1aa4bdc508fc85a98e799e7bf3e18fb6b
matt <matt1235@naver.com>
2024-01-24 19:57:40 +0900

    feat: 이메일로 인증번호를 보내는 로직 구현 (#25) 인증코드 발송 : EmailVerification 엔티티를 생성합니다. JavaMailSender 클래스 인스턴스를 통해 이메일에 인증번호를 보냅니다.

--- api-module/src/main/java/org/devridge/api/domain/emailverification/service/EmailMessageSender.java
+++ api-module/src/main/java/org/devridge/api/domain/emailverification/service/EmailMessageSender.java
@@ -1,4 +1,37 @@
 package org.devridge.api.domain.emailverification.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import javax.mail.internet.MimeMessage;
+
+@Service
 public class EmailMessageSender {
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    @Value("${spring.mail.username}")
+    private String fromEmail;
+
+    public void sendSimpleMessage(String recipientEmail, String emailSubject, String text) {
+        MimeMessage message = javaMailSender.createMimeMessage();
+        try {
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+
+            helper.setFrom(fromEmail);
+            helper.setTo(recipientEmail);
+            helper.setSubject(emailSubject);
+            helper.setText(text, true);
+
+            javaMailSender.send(message);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "이메일 발송에 실패했습니다.", e);
+        }
+    }
 }
---
+++ api-module/src/main/java/org/devridge/api/domain/emailverification/service/EmailVerificationService.java
@@ -0,0 +1,58 @@
+package org.devridge.api.domain.emailverification.service;
+
+import lombok.RequiredArgsConstructor;
+import org.devridge.api.constant.EmailVerificationContentType;
+import org.devridge.api.domain.emailverification.dto.request.EmailRequest;
+import org.devridge.api.domain.emailverification.entity.EmailVerification;
+import org.devridge.api.domain.emailverification.repository.EmailVerificationRepository;
+import org.devridge.api.util.RandomGeneratorUtil;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+public class EmailVerificationService {
+
+    private final EmailVerificationRepository emailVerificationRepository;
+    private final EmailMessageSender emailMessageSender;
+    private final TemplateEngine templateEngine;
+
+    @Value("${devridge.email.expire-minutes.signup}")
+    private final int EMAIL_EXP_MINUTES;
+
+    @Value("${devridge.email.expire-minutes.signup}")
+    private final int VERIFICATION_COMPLETION_EFFECTIVE_TIME;
+
+    public void sendVerificationEmail(EmailRequest emailRequest) {
+        EmailVerification emailVerification = createEmailVerification(emailRequest.getEmail(), EMAIL_EXP_MINUTES);
+        emailVerificationRepository.save(emailVerification);
+
+        Context context = new Context();
+        context.setVariable("verificationCode", emailVerification.getContent());
+        String htmlContent = templateEngine.process("verificationEmailTemplate", context);
+
+        emailMessageSender.sendSimpleMessage(
+                emailVerification.getReceiptEmail(),
+                "[devridge] 가입 인증번호 안내",
+                htmlContent
+        );
+    }
+
+    private EmailVerification createEmailVerification(String email, int expMinutes) {
+        String content = String.valueOf(RandomGeneratorUtil.generateFourDigitNumber());
+        LocalDateTime expiredAt = LocalDateTime.now().plusMinutes(expMinutes);
+
+        return EmailVerification.builder()
+                .receiptEmail(email)
+                .contentType(EmailVerificationContentType.SIGNUP)
+                .content(content)
+                .checkStatus(false)
+                .expireAt(expiredAt)
+                .build();
+    }
+
+}
