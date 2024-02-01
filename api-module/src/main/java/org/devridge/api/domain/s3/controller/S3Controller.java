f4cda03309f9174e43e4fe5659aee223f94aefeb
yu-heejin <jkgt0521@naver.com>
2024-02-02 02:19:47 +0900

    feat: 이미지 삭제 API 구현 (#34)

--- api-module/src/main/java/org/devridge/api/domain/s3/controller/S3Controller.java
+++ api-module/src/main/java/org/devridge/api/domain/s3/controller/S3Controller.java
@@ -2,6 +2,7 @@ package org.devridge.api.domain.s3.controller;
 
 import lombok.RequiredArgsConstructor;
 
+import org.devridge.api.domain.s3.dto.request.DeleteImageRequest;
 import org.devridge.api.domain.s3.dto.response.UploadImageResponse;
 import org.devridge.api.domain.s3.service.S3Service;
 
@@ -11,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.validation.Valid;
 import java.io.IOException;
 
 @RestController
@@ -27,5 +29,11 @@ public class S3Controller {
     ) throws IOException {
         UploadImageResponse response = s3Service.uploadImage(image, path);
         return ResponseEntity.ok().body(response);
+    }
+
+    @DeleteMapping("/images")
+    public ResponseEntity<Void> deleteImage(@RequestBody @Valid DeleteImageRequest imageRequest) {
+        s3Service.deleteImage(imageRequest.getName());
+        return ResponseEntity.ok().build();
     }
 }
