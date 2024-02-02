95a100b751077a9efe9544037b5b9e8d2b173e9f
yu-heejin <jkgt0521@naver.com>
2024-02-02 20:44:02 +0900

    chore: Docker 빌드 방식 변경 (#43)

--- scripts/localbuild.sh
+++ scripts/localbuild.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+source ../.env
 
-git submodule update --init
-../gradlew clean build
+git submodule update --remote --recursive --init
+cd ..
+gradlew clean build
+cd api-module
+java -Djarmode=layertools -jar build/libs/${JAR_NAME} extract
+cd ..
 docker-compose up -d --build
