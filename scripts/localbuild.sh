e711a82c94ba629ea85a815c3bbbbe3b34690854
yu-heejin <jkgt0521@naver.com>
2024-02-02 21:51:38 +0900

    fix: gradlew not found 오류 수정 (#43)

--- scripts/localbuild.sh
+++ scripts/localbuild.sh
@@ -3,7 +3,7 @@ source ../.env
 
 git submodule update --remote --recursive --init
 cd ..
-gradlew clean build
+./gradlew clean build
 cd api-module
 java -Djarmode=layertools -jar build/libs/${JAR_NAME} extract
 cd ..
