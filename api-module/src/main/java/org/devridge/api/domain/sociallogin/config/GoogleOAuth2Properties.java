ceb1986c651a24565f7a42dab9d826afd81e5c99
matt <matt1235@naver.com>
2024-02-03 21:43:41 +0900

    feat: 토큰 strategy의 구현 클래스 생성 (#9) 각 플랫폼마다 OAuth2.0 인가 서버로부터 Token을 가져오는 클래스를 각각 구현합니다.
    clientRegistration 을 통해 설정 파일(yml)에서 매치되는 값들을 불러옵니다.
    단, 구글은 적용이 안되므로 GoogleProperties를 통해, 따로 지정해주었습니다.

---
+++ api-module/src/main/java/org/devridge/api/domain/sociallogin/config/GoogleOAuth2Properties.java
@@ -0,0 +1,17 @@
+package org.devridge.api.domain.sociallogin.config;
+
+import lombok.Getter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "spring.security.oauth2.google")
+@Getter
+public class GoogleOAuth2Properties {
+    private String tokenUri;
+    private String clientId;
+    private String clientSecret;
+    private String redirectUri;
+    private String grantType;
+    private String userInfoEndpoint;
+}
