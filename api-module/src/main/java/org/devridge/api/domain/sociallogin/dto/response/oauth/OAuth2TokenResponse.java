e5be72b222cfe7be70c808603345e86eb74574a7
matt <matt1235@naver.com>
2024-02-04 22:41:32 +0900

    fix: oauth 서버에서 토큰을 받아올 수 있도록 수정 (#9) @JsonProperty를 통해 토큰을 클래스로 받아올 수 있도록 수정하였습니다.

--- api-module/src/main/java/org/devridge/api/domain/sociallogin/dto/response/oauth/OAuth2TokenResponse.java
+++ api-module/src/main/java/org/devridge/api/domain/sociallogin/dto/response/oauth/OAuth2TokenResponse.java
@@ -1,14 +1,26 @@
 package org.devridge.api.domain.sociallogin.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 
 @Getter
 public class OAuth2TokenResponse {
 
+    @JsonProperty("access_token")
     String accessToken;
+
+    @JsonProperty("token_type")
     String tokenType;
+
+    @JsonProperty("refresh_token")
     String refreshToken;
+
+    @JsonProperty("expires_in")
     String expiresIn;
+
+    @JsonProperty("scope")
     String scope;
+
+    @JsonProperty("refresh_token_expires_in")
     String refreshTokenExpiresIn;
 }
