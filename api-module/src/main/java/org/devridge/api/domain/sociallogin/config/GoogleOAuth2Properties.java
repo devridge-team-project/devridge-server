3e86038447b5b9919f37525b409adf793edcdfdb
matt <matt1235@naver.com>
2024-02-04 22:26:31 +0900

    fix: google property 클래스 오류 수정 (#9)

--- api-module/src/main/java/org/devridge/api/domain/sociallogin/config/GoogleOAuth2Properties.java
+++ api-module/src/main/java/org/devridge/api/domain/sociallogin/config/GoogleOAuth2Properties.java
@@ -1,13 +1,16 @@
 package org.devridge.api.domain.sociallogin.config;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ConfigurationProperties(prefix = "spring.security.oauth2.google")
 @Getter
+@Setter
 public class GoogleOAuth2Properties {
+
     private String tokenUri;
     private String clientId;
     private String clientSecret;
