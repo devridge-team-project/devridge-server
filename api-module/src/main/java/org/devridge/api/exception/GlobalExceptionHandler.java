13178f85eb0ecb9f63f74e246b97c4957b427d45
yu-heejin <jkgt0521@naver.com>
2024-02-10 02:04:57 +0900

    feat: @Valid ExceptionHandler 추가 (#40)

--- api-module/src/main/java/org/devridge/api/exception/GlobalExceptionHandler.java
+++ api-module/src/main/java/org/devridge/api/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package org.devridge.api.exception;
 import org.devridge.api.exception.common.BaseException;
 import org.devridge.common.dto.BaseErrorResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -13,6 +14,13 @@ public class GlobalExceptionHandler {
     public ResponseEntity<BaseErrorResponse> handleBaseException(BaseException exception) {
         return ResponseEntity
             .status(exception.getCode())
+            .body(new BaseErrorResponse(exception.getMessage()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<BaseErrorResponse> handleRequestValidException(MethodArgumentNotValidException exception) {
+        return ResponseEntity
+            .status(400)
             .body(new BaseErrorResponse(exception.getMessage()));
     }
 
