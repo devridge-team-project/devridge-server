d60514ee1316c16507aad1dcb9087bbfeec16f52
yu-heejin <jkgt0521@naver.com>
2024-02-12 00:27:49 +0900

    chore: Q&A 세부 조회 불필요한 응답 삭제 (#30)

--- api-module/src/main/java/org/devridge/api/domain/qna/mapper/QnAMapper.java
+++ api-module/src/main/java/org/devridge/api/domain/qna/mapper/QnAMapper.java
@@ -3,15 +3,10 @@ package org.devridge.api.domain.qna.mapper;
 import org.devridge.api.domain.member.entity.Member;
 import org.devridge.api.domain.qna.dto.request.CreateQnARequest;
 import org.devridge.api.domain.qna.dto.response.FindWriterInformation;
-import org.devridge.api.domain.qna.dto.response.GetAllCommentByQnAId;
 import org.devridge.api.domain.qna.dto.response.GetQnADetailResponse;
 import org.devridge.api.domain.qna.entity.QnA;
-import org.devridge.api.domain.qna.entity.QnAComment;
 
 import org.springframework.stereotype.Component;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import static org.devridge.api.domain.qna.mapper.MemberMapper.toMember;
 
@@ -30,7 +25,6 @@ public class QnAMapper {
             .views(result.getViews() + 1)
             .createdAt(result.getCreatedAt())
             .commentCount(result.getComments().size())
-            .comments(this.toComments(result.getComments()))
             .build();
     }
 
@@ -50,24 +44,5 @@ public class QnAMapper {
             .title(qnaRequest.getTitle())
             .content(qnaRequest.getContent())
             .build();
-    }
-
-    private List<GetAllCommentByQnAId> toComments(List<QnAComment> comments) {
-        List<GetAllCommentByQnAId> result = new ArrayList<>();
-
-        for (QnAComment comment : comments) {
-            result.add(
-                GetAllCommentByQnAId.builder()
-                    .id(comment.getId())
-                    .member(toMember(comment.getMember()))
-                    .content(comment.getContent())
-                    .likes(comment.getLikes())
-                    .dislikes(comment.getDislikes())
-                    .createdAt(comment.getCreatedAt())
-                    .build()
-            );
-        }
-
-        return result;
     }
 }
