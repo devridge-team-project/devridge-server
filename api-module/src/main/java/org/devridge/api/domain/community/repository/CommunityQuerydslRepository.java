2b9f8e5ef249152d0939a444a1ce923dabacd4c4
213kky <213kky@naver.com>
2024-02-27 18:06:16 +0900

    fix: 커뮤니티 무한 스크롤 마지막인지 확인 못하는 오류 수정 (#76)

--- api-module/src/main/java/org/devridge/api/domain/community/repository/CommunityQuerydslRepository.java
+++ api-module/src/main/java/org/devridge/api/domain/community/repository/CommunityQuerydslRepository.java
@@ -5,6 +5,7 @@ import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.devridge.api.domain.community.dto.response.CommunitySliceResponse;
@@ -12,6 +13,7 @@ import org.devridge.api.domain.community.dto.response.HashtagResponse;
 import org.devridge.api.domain.community.dto.response.MemberInfoResponse;
 import org.devridge.api.domain.community.entity.QCommunity;
 import org.devridge.api.domain.community.entity.QCommunityHashtag;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.domain.SliceImpl;
@@ -27,60 +29,67 @@ public class CommunityQuerydslRepository {
 
 
     public Slice<CommunitySliceResponse> searchBySlice(Long lastId, Pageable pageable) {
-        List<CommunitySliceResponse> results = jpaQueryFactory
-            .selectFrom(community)
-            .leftJoin(community.member)
-            .leftJoin(community.hashtags, communityHashtag)
-            .leftJoin(communityHashtag.hashtag)
-            .where(
-                ltId(lastId),
-                community.isDeleted.eq(false)
-            )
-            .orderBy(community.id.desc())
-            .limit(pageable.getPageSize() + 1)
-            .transform(GroupBy.groupBy(community.id)
-                .list(Projections.constructor(
-                    CommunitySliceResponse.class,
-                    community.id,
-                    community.title,
-                    community.views,
-                    community.likeCount,
-                    community.comments.size().longValue(),
+        List<CommunitySliceResponse> results = new ArrayList<>();
 
-                    Projections.constructor(
-                        MemberInfoResponse.class,
-                        Expressions.cases()
-                            .when(community.member.nickname.isNull())
-                            .then(0L)
-                            .otherwise(community.member.id),
-                        Expressions.cases()
-                            .when(community.member.nickname.isNull())
-                            .then("DefaultNickname")
-                            .otherwise(community.member.nickname),
-                        Expressions.cases()
-                            .when(community.member.nickname.isNull())
-                            .then("DefaultUrl")
-                            .otherwise(community.member.profileImageUrl),
-                        Expressions.cases()
-                            .when(community.member.nickname.isNull())
-                            .then("DefaultIntroduction")
-                            .otherwise(community.member.introduction)
-                    ),
+        while (results.size() <= 10) {
+            List<CommunitySliceResponse> result = jpaQueryFactory
+                .selectFrom(community)
+                .leftJoin(community.member)
+                .leftJoin(community.hashtags, communityHashtag)
+                .leftJoin(communityHashtag.hashtag)
+                .where(
+                    ltId(lastId),
+                    community.isDeleted.eq(false)
+                )
+                .orderBy(community.id.desc())
+                .limit(pageable.getPageSize() + 1)
+                .transform(GroupBy.groupBy(community.id)
+                    .list(Projections.constructor(
+                        CommunitySliceResponse.class,
+                        community.id,
+                        community.title,
+                        community.views,
+                        community.likeCount,
+                        community.comments.size().longValue(),
 
-                    community.createdAt,
-                    community.updatedAt,
+                        Projections.constructor(
+                            MemberInfoResponse.class,
+                            Expressions.cases()
+                                .when(community.member.nickname.isNull())
+                                .then(0L)
+                                .otherwise(community.member.id),
+                            Expressions.cases()
+                                .when(community.member.nickname.isNull())
+                                .then("DefaultNickname")
+                                .otherwise(community.member.nickname),
+                            Expressions.cases()
+                                .when(community.member.nickname.isNull())
+                                .then("DefaultUrl")
+                                .otherwise(community.member.profileImageUrl),
+                            Expressions.cases()
+                                .when(community.member.nickname.isNull())
+                                .then("DefaultIntroduction")
+                                .otherwise(community.member.introduction)
+                        ),
 
-                    GroupBy.list(Projections.fields(
-                        HashtagResponse.class,
-                        communityHashtag.hashtag.id,
-                        communityHashtag.hashtag.word,
-                        communityHashtag.hashtag.count
+                        community.createdAt,
+                        community.updatedAt,
+
+                        GroupBy.list(Projections.fields(
+                            HashtagResponse.class,
+                            communityHashtag.hashtag.id,
+                            communityHashtag.hashtag.word,
+                            communityHashtag.hashtag.count
                         )),
 
-                    community.scraps.size().longValue()
-                ))
-            );
-        return checkLastPage(pageable, results);
+                        community.scraps.size().longValue()
+                    ))
+                );
+            results.addAll(result);
+        }
+
+        Pageable customPageable = PageRequest.of(0 , results.size() - 1, pageable.getSort());
+        return checkLastPage(customPageable, results);
     }
 
     // no-offset 방식 처리하는 메서드
