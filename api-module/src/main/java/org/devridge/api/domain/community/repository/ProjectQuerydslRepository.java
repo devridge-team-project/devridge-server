a7aab7857e7ff9445608386af375e45920fdca99
213kky <213kky@naver.com>
2024-02-27 18:17:05 +0900

    fix: 프로젝트 무한 스크롤 마지막인지 확인 못하는 오류 수정 (#76)

--- api-module/src/main/java/org/devridge/api/domain/community/repository/ProjectQuerydslRepository.java
+++ api-module/src/main/java/org/devridge/api/domain/community/repository/ProjectQuerydslRepository.java
@@ -4,12 +4,14 @@ import com.querydsl.core.group.GroupBy;
 import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.devridge.api.domain.community.dto.response.ProjectListResponse;
 import org.devridge.api.domain.community.entity.QProject;
 import org.devridge.api.domain.skill.entity.QProjectSkill;
 import org.devridge.api.domain.skill.entity.QSkill;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.domain.SliceImpl;
@@ -26,34 +28,41 @@ public class ProjectQuerydslRepository {
 
 
     public Slice<ProjectListResponse> searchBySlice(Long lastId, Pageable pageable) {
-        List<ProjectListResponse> results = jpaQueryFactory
-            .selectFrom(project)
-            .leftJoin(project.member)
-            .leftJoin(project.projectSkills, projectSkill)
-            .leftJoin(projectSkill.skill, skill)
-            .where(
-                ltId(lastId),
-                project.isDeleted.eq(false)
-            )
-            .orderBy(project.id.desc())
-            .limit(pageable.getPageSize() + 1)
-            .transform(GroupBy.groupBy(project.id).
-                list(Projections.constructor(
-                    ProjectListResponse.class,
-                    project.id,
-                    project.category,
-                    project.title,
-                    project.content,
-                    project.likes,
-                    project.dislikes,
-                    project.views,
-                    project.isRecruiting,
-                    GroupBy.list(skill.skill),
-                    project.meeting
-            )));
+        List<ProjectListResponse> results = new ArrayList<>();
+
+        while (results.size() <= 10) {
+            List<ProjectListResponse> result = jpaQueryFactory
+                .selectFrom(project)
+                .leftJoin(project.member)
+                .leftJoin(project.projectSkills, projectSkill)
+                .leftJoin(projectSkill.skill, skill)
+                .where(
+                    ltId(lastId),
+                    project.isDeleted.eq(false)
+                )
+                .orderBy(project.id.desc())
+                .limit(pageable.getPageSize() + 1)
+                .transform(GroupBy.groupBy(project.id).
+                    list(Projections.constructor(
+                        ProjectListResponse.class,
+                        project.id,
+                        project.category,
+                        project.title,
+                        project.content,
+                        project.likes,
+                        project.dislikes,
+                        project.views,
+                        project.isRecruiting,
+                        GroupBy.list(skill.skill),
+                        project.meeting
+                    )));
+            results.addAll(result);
+        }
 
 
-        return checkLastPage(pageable, results);
+
+        Pageable customPageable = PageRequest.of(0 , results.size() - 1, pageable.getSort());
+        return checkLastPage(customPageable, results);
     }
 
     // no-offset 방식 처리하는 메서드
