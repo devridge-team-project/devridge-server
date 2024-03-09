a8b8a4114a8cfe81b987770c35acaebbbe03d2fe
matt <matt1235@naver.com>
2024-03-10 00:05:32 +0900

    feat: 에러 클래스 추가 (#81)

--- api-module/src/main/java/org/devridge/api/exception/custom/TokenInvalidException.java
+++ api-module/src/main/java/org/devridge/api/exception/custom/TokenInvalidException.java
@@ -3,6 +3,10 @@ package org.devridge.api.exception.common;
 public class TokenInvalidException extends BaseException {
 
     public TokenInvalidException() {
-        super(403, "Invalid Token!");
+        super(401, "Invalid Token!");
+    }
+
+    public TokenInvalidException(String message) {
+        super(401, message);
     }
 }
