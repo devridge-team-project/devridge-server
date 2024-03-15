43ac1036695eaa8947c22c6eddfaa6ef4b0911b4
213kky <213kky@naver.com>
2024-03-15 16:41:51 +0900

    rename: 파일 이름 변경 (#77)

--- api-module/src/main/java/org/devridge/api/presentation/controller/note/ParticipationNoteController.java
+++ api-module/src/main/java/org/devridge/api/presentation/controller/note/ParticipationNoteController.java
@@ -24,7 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/notes/participation")
-public class ProjectParticipationNoteController {
+public class ParticipationNoteController {
 
     private final ParticipationNoteService participationNoteService;
 
