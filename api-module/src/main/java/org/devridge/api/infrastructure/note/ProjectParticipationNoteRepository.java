6bcb8681e260537385e0543c58df09d4036bd051
213kky <213kky@naver.com>
2024-03-20 21:12:51 +0900

    refactor: 불필요한 코드 삭제 (#77)

--- api-module/src/main/java/org/devridge/api/infrastructure/note/ProjectParticipationNoteRepository.java
+++
@@ -1,7 +0,0 @@
-package org.devridge.api.infrastructure.note;
-
-import org.devridge.api.domain.note.entity.ProjectParticipationNote;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProjectParticipationNoteRepository extends JpaRepository<ProjectParticipationNote, Long> {
-}
--- api-module/src/main/java/org/devridge/api/infrastructure/note/StudyParticipationNoteRepository.java
+++
@@ -1,8 +0,0 @@
-package org.devridge.api.infrastructure.note;
-
-import org.devridge.api.domain.note.entity.StudyParticipationNote;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudyParticipationNoteRepository extends JpaRepository<StudyParticipationNote, Long> {
-
-}
