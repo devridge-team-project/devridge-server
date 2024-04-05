45c0c76cdbef913c85df4213300fb1d43e3b1ed6
213kky <213kky@naver.com>
2024-04-05 22:17:10 +0900

    refactor: 불필요한 코드 제거 (#118)

--- api-module/src/main/java/org/devridge/api/application/community/project/ProjectCommentMapper.java
+++ api-module/src/main/java/org/devridge/api/application/community/project/ProjectCommentMapper.java
@@ -1,6 +1,5 @@
 package org.devridge.api.application.community.project;
 
-import lombok.RequiredArgsConstructor;
 import org.devridge.api.domain.community.dto.request.ProjectCommentRequest;
 import org.devridge.api.domain.community.entity.Project;
 import org.devridge.api.domain.community.entity.ProjectComment;
@@ -8,7 +7,6 @@ import org.devridge.api.domain.member.entity.Member;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class ProjectCommentMapper {
 
     public ProjectComment toProjectComment(Project project, Member member, ProjectCommentRequest Request) {
