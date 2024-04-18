47366a4c93988db143920d8ded7d6978eabcc723
matt <matt1235@naver.com>
2024-04-18 18:08:35 +0900

    feat: 쿠키에서 JWT 가져오는 로직 추가 (#137)

--- api-module/src/main/java/org/devridge/api/common/util/AccessTokenUtil.java
+++ api-module/src/main/java/org/devridge/api/common/util/AccessTokenUtil.java
@@ -2,9 +2,9 @@ package org.devridge.api.common.util;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-
 import org.devridge.api.common.security.auth.AuthProperties;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
 public class AccessTokenUtil {
@@ -19,5 +19,18 @@ public class AccessTokenUtil {
             return bearerToken.substring(7).trim();
         }
         return null;
+    }
+
+    public static String extractAccessTokenFromCookies(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if ("dev-access".equals(cookie.getName())) {
+                    return cookie.getValue();
+                }
+            }
+        }
+        return null; // 해당 이름의 쿠키가 없을 경우 null을 반환
     }
 }
